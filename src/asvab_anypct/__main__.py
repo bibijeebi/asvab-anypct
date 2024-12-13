e634ca1ccb886e9ae25c2b32476fee266827b735
bibijeebi <bennyforeman1@gmail.com>
2024-12-13 05:51:54 +0000

    Add new dependencies and implement main application logic
    - Added 'gumpython' and 'sh' as dependencies in pyproject.toml and uv.lock.
    - Created __main__.py to implement the main application logic for a quiz app, allowing users to select difficulty and topic for questions.
    - Updated file permissions for __main__.py to make it executable.

---
+++ src/asvab_anypct/__main__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from typing_extensions import Literal
+from cyclopts import App
+from sh import gum
+
+app = App()
+
+difficulty_choices = Literal["easy", "medium", "hard"]
+topic_choices = Literal["math", "vocab"]
+
+easy_question_count = 10
+medium_question_count = easy_question_count * 2
+hard_question_count = medium_question_count * 2
+
+@app.default
+def main(
+    difficulty: difficulty_choices | None = None,
+    topic: Literal["math", "vocab"] | None = None,
+    num_questions: int | None = None,
+):
+    if difficulty is None:
+        # print("Choose difficulty: easy, medium, hard")
+        # difficulty = input("> ")
+
+
+    if topic is None:
+        # print("Choose topic: math, vocab")
+        # topic = input("> ")
+
+    if num_questions is None:
+
+    if difficulty == "easy":
+        num_questions = easy_question_count
+    elif difficulty == "medium":
+        num_questions = medium_question_count
+    elif difficulty == "hard":
+        num_questions = hard_question_count
+
+    print(f"Quizzing {topic} with {num_questions} questions of difficulty {difficulty}")
+
+if __name__ == "__main__":
+    app()
+
